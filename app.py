0b3702db7ca4d8fa5500e83fdf7fcef5c66b2bcc
astnne <astnne09@gmail.com>
2024-02-01 13:50:58 -0800

    commit

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  cardInfos = [
-    ["https://i.imgur.com/2BMBtLz.png", 
-     "Cubing", 
-     "Austin moves his fingers.", 
-    "https://www.worldcubeassociation.org/persons/2023NGAU01", 
-     "View Austin's WCA Profile", 
-     "/cubing",
-     "January 18, 2024 at 1:20AM"], #cubing
-    ["https://i.imgur.com/1481ERD.jpg",
-     "Languages",
-     "Austin loves to speak Chinese.",
-     "https://upimg.baike.so.com/doc/4160593-4360651.html",
-     "View About Austin's School in China",
-     "January 18, 2024 at 1:28AM"], #languages
-    ["https://i.imgur.com/VHHLDjr.png",
-     "Programming",
-     "Austin moves his fingers. Again.",
-     "https://astnne.github.io/",
-     "View Austin's Other Website",
-     "January 18, 2024 at 1:33AM"] #programming
-  ]
-
+  cardInfos = json.load(open('./templates/json/cardInfos.json'))
+  print(cardInfos)
   return render_template('index.html', cardInfos=cardInfos)
 
 @app.route('/cubing')
