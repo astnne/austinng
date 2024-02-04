097d2033ac681b2b88e1090433d7f9b37caf5a3b
astnne <astnne09@gmail.com>
2024-02-04 14:58:56 -0800

    commit

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  header = json.load(open('./templates/json/index/header.json'))
-  cardInfos = json.load(open('./templates/json/index/cardInfos.json'))
-  featured = json.load(open('./templates/json/index/featured.json'))
-  return render_template('index.html', header=header, cardInfos=cardInfos, featured=featured)
+  header = json.load(open('./templates/json/index/header/header.json'))
+  cards = json.load(open('./templates/json/index/content/cards.json'))
+  featured = json.load(open('./templates/json/index/content/featured.json'))
+  return render_template('index.html', header=header, cards=cards, featured=featured)
 
-@app.route('/cubing')
+@app.route('/speedcubing')
 def cubing():
-  return render_template('cubing.html')
+  header = json.load(open('./templates/json/speedcubing/header/header.json'))
+  cards = json.load(open('./templates/json/speedcubing/content/cards.json'))
+  return render_template('speedcubing.html', header=header, cards=cards)
 
 @app.route('/languages')
 def languages():
